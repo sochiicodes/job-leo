5cb77444c8ec45a2503ae7c923b2a14da8a41026
SochiCodes <sochiicodes@gmail.com>
2026-08-08 00:54:37 +0100

    style: add style category pill

--- src/components/common/CategoryPill.tsx
+++ src/components/common/CategoryPill.tsx
@@ -1,9 +1,19 @@
-import { Dot } from "lucide-react"
+interface CategoryPillProp{
+    cat: string
+}
 
-const CategoryPill = ({cat}) =>{
+const CategoryPill = ({cat}: CategoryPillProp) =>{
+    const selectedCategory = 'Design'
     return(
-        <button className=" flex py-2 px-4 border border-gray-200 rounded-3xl justify-center align-center text-gray-400">
-            <Dot/> 
+        <button
+            key={cat}
+            className={`group flex items-center gap-2 px-5 py-2 space-x-2 whitespace-nowrap rounded-2xl text-xs border transition-all ${
+                selectedCategory === cat
+                ? 'bg-brand-500 text-white border-brand-500 shadow-xl shadow-brand-100'
+                : 'bg-white text-slate-500 border-slate-200 hover:border-brand-400'
+            }`}
+        >
+            <span className="flex rounded-full h-2 w-2 bg-slate-500 group-hover:bg-brand-400"></span>
             {cat}
         </button>
     )
--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -6,7 +6,7 @@ export default function JobsPage(){
     return(
         <main className="pt-16">
             <HeroJobSection/>
-            <section className="w-full flex overflow-hidden p-4 gap-6">
+            <section className="w-full flex justify-center align-center overflow-hidden p-4 gap-2 ">
                 {
                     JOB_CATEGORIES.map((cat: string, index) =>(
                         <CategoryPill cat={cat} key={index}/>
