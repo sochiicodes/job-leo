cb964e715a13502aeb8d5783fe40319fd33f0d6e
SochiCodes <sochiicodes@gmail.com>
2026-08-07 17:39:11 +0100

    Style: add Job Hero section

--- src/features/jobs/components/HeroJobSection.tsx
+++ src/features/jobs/components/HeroJobSection.tsx
@@ -5,16 +5,18 @@ import HeroJobStats from "./HeroJobStats"
 
 export const HeroJobSection = () =>{
     return(
-        <section className="relative w-full h-[335px]  bg-gradient-to-br from-slate-900 via-slate-800 via-60% to-slate-700 text-white justify-center align-center">
+        <section className="relative w-full h-[335px]  bg-gradient-to-br from-slate-900 via-slate-800 via-60% to-slate-700 flex justify-center align-center gap-8">
             <img alt="" aria-hidden="true" className="absolute inset-0 bg-[url('/crosspattern.svg')]"/>
-            <div>
-                <h1>Browse all opportunities</h1>
-                <h1>Find your next great role</h1>
-                <p>Thousands of jobs from top companies, updated daily.</p>
-            </div>
-            <div className="flex flex-col gap-5">
-                <HeroJobSearch/>
-                <HeroJobStats/>
+            <div className="max-w-7xl flex flex-col justify-center align-start text-white">
+                <div className="flex flex-col gap-4">
+                    <h1 className="text-sm text-gray-400">Browse all opportunities</h1>
+                    <h1 className="text-6xl font-bold">Find your next great role</h1>
+                    <p className="text-m text-gray-400">Thousands of jobs from top companies, updated daily.</p>
+                </div>
+                <div className="flex flex-col gap-4 w-max-7xl">
+                    <HeroJobSearch/>
+                    <HeroJobStats/>
+                </div>
             </div>
         </section>
     )
--- src/features/jobs/components/HeroJobStats.tsx
+++ src/features/jobs/components/HeroJobStats.tsx
@@ -2,15 +2,15 @@ import { SearchIcon } from "lucide-react"
 
 const HeroJobStats = () =>{
     return(
-        <section className="flex gap-10">
+        <section className="flex gap-10 align-start">
             <p className="text-xs text-white font-bold">
-                63 <span className="text-xs text-gray-200 font-normal">jobs found</span>
+                63 <span className="text-xs text-gray-400 font-normal">jobs found</span>
             </p>
             <p className="text-xs text-white font-bold">
-                380+ <span className="text-xs text-gray-200 font-normal">companies hiring</span>
+                380+ <span className="text-xs text-gray-400 font-normal">companies hiring</span>
             </p>
             <p className="text-xs text-white font-bold">
-                63 <span className="text-xs text-gray-200 font-normal">New jobs added today</span>
+                63 <span className="text-xs text-gray-400 font-normal">New jobs added today</span>
             </p>
         </section>
     )
