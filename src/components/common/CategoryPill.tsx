c40977001a8fe32cf10e46252f43a87a3d777291
SochiCodes <sochiicodes@gmail.com>
2026-08-08 00:08:06 +0100

    feat: add style category pill

--- src/components/common/CategoryPill.tsx
+++ src/components/common/CategoryPill.tsx
@@ -1,9 +1,19 @@
-import { Dot } from "lucide-react"
 
-const CategoryPill = ({cat}) =>{
+interface CategoryPillProps{
+    cat: string
+}
+
+const CategoryPill = ({cat}: CategoryPillProps) =>{
+    const selectedCategory = "Design"
     return(
-        <button className=" flex py-2 px-4 border border-gray-200 rounded-3xl justify-center align-center text-gray-400">
-            <Dot/> 
+        <button
+            key={cat}
+            className={`px-2 py-2 rounded-2xl text-xs border transition-all ${
+                selectedCategory === cat
+                ? 'bg-brand-500 text-white border-brand-500 shadow-xl shadow-brand-100'
+                : 'bg-white text-slate-500 border-slate-200 hover:border-brand-400'
+            }`}
+            >
             {cat}
         </button>
     )
