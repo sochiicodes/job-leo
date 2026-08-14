1b2c3ee2332cb62c958744eb783bbad3d420cd91
SochiCodes <sochiicodes@gmail.com>
2026-08-14 01:30:22 +0100

    feat: connect filter chips to zustand

--- src/components/common/ActiveFilterChip.tsx
+++ src/components/common/ActiveFilterChip.tsx
@@ -1,15 +1,19 @@
 interface ActiveFilterChipProps{
     label: string
+    onRemove: () => void
 }
 
-const ActiveFilterChip = ({label}: ActiveFilterChipProps) => {
+const ActiveFilterChip = ({label, onRemove}: ActiveFilterChipProps) => {
   
     return (
         <button
             className="flex items-center gap-3 bg-brand-light text-brand-dark border border-green-200 rounded-full text-xs font-medium px-3 py-1 hover:bg-green-100 transition-colors group"
         >
             {label}
-        <span className="text-brand-slate/60 text-xs group-hover:text-brand-900 transition-colors ">✕</span>
+        <span 
+            className="text-brand-slate/60 text-xs group-hover:text-brand-900 transition-colors "
+            onClick={onRemove}
+        >✕</span>
         </button>
     );
   }
--- src/features/jobs/components/JobLists.tsx
+++ src/features/jobs/components/JobLists.tsx
@@ -1,9 +1,8 @@
 import JobCard from "./JobCard";
 import type { ViewMode } from "../types";
 import { VIEW_MODE } from "../../../constants/jobs";
-import { useStore } from "../../../store/store";
-import { useShallow } from "zustand/shallow";
 import { ActiveFilterChip, SortDropDown } from "../../../components";
+import { useJobFilters } from "../useJobFilters";
 
 const GRID_LAYOUT: Record<ViewMode, string> = {
     list: "grid-cols-1",
@@ -11,14 +10,58 @@ const GRID_LAYOUT: Record<ViewMode, string> = {
 };
 
 const JobLists = () => {
-    const {viewMode, setViewMode} = useStore(
-        useShallow((state) =>({
-            viewMode: state.viewMode,
-            setViewMode: state.setViewMode
-        }))
-    )
+    const {
+        viewMode, 
+        setViewMode,
+        category,
+        setCategory,
+        jobType,
+        setJobType,
+        expLevel,
+        setExpLevel,
+        location,
+        setLocation,
+        datePosted,
+        setDatePosted,
+        jobStatus,
+        setJobStatus,
+    } = useJobFilters()
     // const viewMode: ViewMode = "list";
     const filterLength = 20
+
+    const chips = [
+        ...(category !== "All"
+            ? [{label: category, onRemove: () => setCategory("All")}]
+            : []),
+        
+            ...jobType.map((type) =>({
+                label: type,
+                onRemove: () => setJobType(type)
+            })),
+
+            ...expLevel.map((level) =>({
+                label: level,
+                onRemove: () => setExpLevel(level)
+            })),
+
+            ...location.map((loc) =>({
+                label: loc,
+                onRemove: () => setLocation(loc)
+            })),
+
+            ...(datePosted !== "any" 
+                ? [{
+                    label: `Last ${datePosted} ${datePosted === "1" ? "day" : " days"}`,
+                    onRemove: () => setDatePosted("any")
+                }] 
+                : []),
+            
+            // ...jobStatus?.map((status) =>({
+            //     label: status,
+            //     onRemove: () => setJobStatus(status)
+            // }))
+    ]
+
 
     return (
         <section className="grow min-w-0 flex flex-col gap-4">
@@ -53,8 +96,15 @@ const JobLists = () => {
             {/* Filter Chips */}
             <section className="flex flex-wrap gap-4">
                 {
-                ["Design", "Tech", "Beauty"].map((label) =>(
-                    <ActiveFilterChip label={label}/>
+                // ["Design", "Tech", "Beauty"].map((label) =>(
+                //     <ActiveFilterChip label={label}/>
+                // ))
+                chips.map((chip) =>(
+                    <ActiveFilterChip
+                        key={chip.label} 
+                        label={chip.label}
+                        onRemove={chip.onRemove}
+                    />
                 ))
                 }
 
