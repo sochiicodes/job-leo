cb01f2f87b7becbb67de58394142c3aca332e8e2
SochiCodes <sochiicodes@gmail.com>
2026-08-22 17:48:59 +0100

    fix: delete JobsSection to resolve Vercel deploy error

--- frontend/src/features/home/index.ts
+++ frontend/src/features/home/index.ts
@@ -1,5 +1,4 @@
 export { default as Hero } from "./components/Hero";
-export { default as JobsSection } from "./components/JobsSection";
 export { default as ProcessSection } from "./components/ProcessSection";
 export { default as RegisterSection } from "./components/RegisterSection";
 export { default as FAQSection } from "./components/FAQSection";
